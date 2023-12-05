900de55030e0d5bb447a85a357ce1cd824be138e
ntokozo705 <ntokozo705@gmail.com>
2023-12-05 13:04:11 -0800

    done

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
-import string
-print(string.ascii_uppercase)
+from magic_calculation_102 import add, sub
+
+
+def magic_calculation(a, b):
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+    else:
+        return sub(a, b)
